f74f369be9896a30edad7f6b048f30d50eab9bb2
dimaTidev <timofeenkodima@gmail.com>
2021-10-16 19:00:00 +0200

    init

---
+++ Runtime/ArrayList_Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ArrayList_Extensions
+{
+    public static bool Exist<T>(this ICollection<T> list) => list != null && list.Count > 0;
+    public static bool InRange<T>(this ICollection<T> list, int id) => list != null && id >= 0 && id < list.Count;
+
+  //  public static bool InRange<T>(this List<T> list, int id) => list != null && id >= 0 && id < list.Count;
+  //
+  //  public static bool InRange(this Array array, int id) => array != null && id >= 0 && id < array.Length;
+}
