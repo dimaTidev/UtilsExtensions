2888ba1eb13ef0a17878198c93ff8e206723c24c
dimaTidev <timofeenkodima@gmail.com>
2021-10-28 15:55:05 +0200

    Added flags and debug

---
+++ Runtime/Debug_Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Log
+{
+    public static void Collection<T>(ICollection<T> list, string prefix = "Collection debug") where T : IConvertible
+    {
+        string deb = "";
+        foreach (var item in list)
+            deb += item.ToString() + "\n";
+
+        Debug.Log(prefix + ": " + deb);
+    }
+}
---
+++ Runtime/UtilFlags.cs
@@ -0,0 +1,58 @@
+﻿//Info reference https://stackoverflow.com/questions/3261451/using-a-bitmask-in-c-sharp
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilFlags
+{
+   // [Flags]
+   // public enum Names
+   // {
+   //     None = 0,
+   //     Susan = 1,
+   //     Bob = 2,
+   //     Karen = 4
+   // }
+
+    public static bool IsSet<T>(T flags, T flag) where T : struct
+    {
+        int flagsValue = (int)(object)flags;
+        int flagValue = (int)(object)flag;
+
+        return (flagsValue & flagValue) != 0;
+    }
+
+    public static void Set<T>(ref T flags, T flag) where T : struct
+    {
+        int flagsValue = (int)(object)flags;
+        int flagValue = (int)(object)flag;
+
+        flags = (T)(object)(flagsValue | flagValue);
+    }
+  
+    public static void Unset<T>(ref T flags, T flag) where T : struct
+    {
+        int flagsValue = (int)(object)flags;
+        int flagValue = (int)(object)flag;
+
+        flags = (T)(object)(flagsValue & (~flagValue));
+    }
+
+    /// <summary>
+    ///Do a opposite action (bool isTrue = !isTrue)
+    /// </summary>
+    public static bool NegativeSet<T>(ref T flags, T flag) where T : struct
+    {
+        if (IsSet(flags, flag))
+        {
+            Unset(ref flags, flag);
+            return false;
+        }
+        else
+        {
+            Set(ref flags, flag);
+            return true;
+        }    
+    }
+
+}
